b120323d53f12bf591ad6d02e220e4a99367fb2d
Elizabethucar <elizg77@hotmail.com>
2022-09-11 00:21:33 +0200

    working routes

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express= require("express")
 const app = express()
 const PORT = process.env.PORT || 3000
 app.use(express.json())
+app.use('/api/user', require("./routes/user_v0.js"))
 
 app.listen(PORT,()=>{
 	console.log(`listening on port ${PORT}`)
 })
 
-
+	
